48d648be12885499dbc1c93976a587d27a90af90
Steven <steven.nanogame@gmail.com>
2025-02-22 15:18:55 -0500

    Mis à jour le vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/huskies5439.github.io/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
